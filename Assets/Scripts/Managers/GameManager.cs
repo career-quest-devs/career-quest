6fa67e881f6d3bd6b24462b0d00bbdff4c18f27c
M3 <90028264+yinyaozhu@users.noreply.github.com>
2024-12-02 21:57:28 -0500

    update for art assets

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool isPlaying;
+
+    /// <summary>
+    /// Singleton -------------------
+    /// </summary>
+    private static GameManager instance;
+
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+
+    void SetSingleton()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    private void Awake()
+    {
+        SetSingleton();
+    }
+
+    /// <summary>
+    /// End of the Singleton -------------------
+    /// </summary>
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){ //Play
+            StartGame();
+        }
+    }
+
+    public void StartGame()
+    {
+        isPlaying = true;
+
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void helloWorld() {
+        Debug.Log("Hello world.");
+    }
+}
