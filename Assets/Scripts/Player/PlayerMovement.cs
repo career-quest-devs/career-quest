d18be914dffa7f840b59c0c7e2470febf6b16a5a
M3 <90028264+yinyaozhu@users.noreply.github.com>
2024-12-04 10:11:34 -0500

    Add player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 5.0f;
+    [SerializeField] private Animator Default;
 
     private Vector2 _moveInput;
     private Rigidbody2D _rb;
@@ -30,5 +31,20 @@ public class PlayerMovement : MonoBehaviour
     private void MovePlayer()
     {
         _rb.velocity = new Vector2(_moveInput.x * _moveSpeed, _rb.velocity.y);
+
+        if (_moveInput.x > 0)
+        {
+            Default.SetBool("isWalking", true);
+            Default.SetBool("isGoingRight", true);
+        } else if (_moveInput.x < 0)
+        {
+            Default.SetBool("isWalking", true);
+            Default.SetBool("isGoingRight", false);
+        }
+        else
+        {
+            Default.SetBool("isWalking", false);
+        }
+
     }
 }
