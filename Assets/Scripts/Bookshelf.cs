21c1f381555dfae6357bfb9ab790207afd7dc838
garethcheah <garethcheah@gmail.com>
2024-12-05 05:29:47 -0800

    Add dust particles to bookshelf that trigger when player is in vicinity

---
+++ Assets/Scripts/Bookshelf.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bookshelf : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _dustParticles;
+
+    private BoxCollider2D _boxCollider;
+
+    private void Start()
+    {
+        _boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !_dustParticles.isPlaying)
+        {
+            _dustParticles.Play();
+            _boxCollider.enabled = false;
+        }
+    }
+}
