caab4aea1b836cb516fd52f3c7ff2b0e80acc4da
Noelioli <noeltntnoel@gmail.com>
2024-12-15 19:09:31 -0700

    Scene Change, with mini level

---
+++ Assets/Scripts/Helpers/SceneChange.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    /// <summary>
+    /// Singleton -------------------
+    /// </summary>
+    private static SceneChange instance;
+
+    public static SceneChange GetInstance()
+    {
+        return instance;
+    }
+
+    void SetSingleton()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    /// <summary>
+    /// End of the Singleton -------------------
+    /// </summary>
+
+    void Awake()
+    {
+        SetSingleton();
+        DontDestroyOnLoad(this);
+    }
+
+    [SerializeField] private int miniLevelIndex;
+
+    DataTracker data;
+    int lastScene;
+    List<int> loadNext = new List<int> { 0,1 };
+
+    private void Start()
+    {
+        data = DataTracker.GetInstance();
+    }
+
+    public void NextScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        int current = currentScene.buildIndex;
+        
+        lastScene = current;
+        if (loadNext.Contains(current))
+        {
+            // Fade to black
+            SceneManager.LoadScene(current++);
+        }
+        else
+        {
+            // Mini Level
+            SceneManager.LoadScene(miniLevelIndex);
+        }
+    }
+}
