ab8ecd4921e95eab5032a06050c4a44a7ed69c70
M3 <90028264+yinyaozhu@users.noreply.github.com>
2024-12-21 10:29:55 -0500

    mini level icon

--- Assets/Scripts/Managers/Level2Manager.cs
+++ Assets/Scripts/Managers/Level2Manager.cs
@@ -7,12 +7,13 @@ public class Level2Manager : MonoBehaviour
     [SerializeField] private UIManager _uIManager;
     [SerializeField] private PlayerController _player;
     [SerializeField] private PlayerSneeze _playerSneeze;
+    [SerializeField] private PlayerWave _playerWave;
 
 
     // Dialog collection
     private string[] _introDialog = new string[2]
     {
-        "Alex: I got to be hurry.",
+        "Alex: I got to be hurry!",
         "Alex: I remember the evalator is on the right."
     };
 
@@ -20,7 +21,23 @@ public class Level2Manager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            //Set visibility for mobile action buttons to false
+
+            //_uIManager.SetSneezeButtonVisibility(false);
+            _uIManager.SetWaveButtonVisibility(false);
+            _uIManager.SetDashButtonVisibility(false);
+            _uIManager.SetPickUpButtonVisibility(false);
+            _uIManager.SetOpenButtonVisibility(false);
+        }
+
+        StartIntroDialog();
+    }
+    private void StartIntroDialog()
+    {
+        _player.SwitchToUIActionMap();
+        _uIManager.StartDialog(_introDialog);
     }
 
     // Update is called once per frame
